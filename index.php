f5b2c35741d35432b1797956646de2f89cf7fdac
mateusmachado3594@gmail.com <mateusmachado3594@gmail.com>
2022-01-10 21:57:56 -0300

    Ajuste

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<PHP 
-    echo "<h1>Hello World!<h1>";
+<?PHP 
+    echo "<h1> Hello World! <h1>";
 
     echo "My Name is Mateus Machado<br>";
 
     Echo "Sou desenvolvedor";
 
     print("<p>Hoje é segunda<p>");
->
+
+?>
